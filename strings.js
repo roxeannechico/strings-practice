456285d66194f4596a634db9853f98d2ca9a4061
roxeannechico <roseannechico22@gmail.com>
2024-09-14 21:03:44 +0800

    initial commit

---
+++ strings.js
@@ -0,0 +1,16 @@
+let favActorFirstName = 'Joshua';
+let favActorLastName = 'Garcia';
+let fullName = favActorFirstName + ' ' + favActorLastName; 
+
+let uppercase = 'joshua garcia';
+let lowercase = 'TIGRE';
+
+let message = 'My favorite actor is JOSHUA GARCIA';
+message += ' his best show is Silicon Valley';
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
